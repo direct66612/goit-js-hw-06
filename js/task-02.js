26d5a79d518959371511bdf71bce9c818fa1818d
direct66612 <direct66612@gmail.com>
2023-08-22 16:19:13 -0400

    complete task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const content = document.querySelector("#ingredients");
+ingredients.forEach((elem) => {
+  const list = document.createElement("li");
+  list.textContent = elem;
+  list.classList.add("item");
+  content.append(list);
+});
